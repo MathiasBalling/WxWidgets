72a7de0c374a84221ea303c72ece00f205a265d8
Mathias Balling <math260r@gmail.com>
2023-03-31 22:22:54 +0200

    31-03-2023 - 22:22

--- Header/ColorPane.h
+++ Header/ColorPane.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "SelectablePane.h"
+#include "selectablepane.h"
 
 class ColorPane : public SelectablePane
 {
@@ -12,3 +12,16 @@ public:
 private:
     virtual void DrawContent(wxGraphicsContext *gc, const wxRect &rect, int roundness) const override;
 };
+
+ColorPane::ColorPane(wxWindow *parent, wxWindowID id, const wxColor &paneColor, const wxPoint &pos, const wxSize &size)
+    : SelectablePane(parent, id, pos, size), color(paneColor)
+{
+}
+
+void ColorPane::DrawContent(wxGraphicsContext *gc, const wxRect &rect, int roundness) const
+{
+    gc->SetPen(wxPen(color));
+    gc->SetBrush(wxBrush(color));
+
+    gc->DrawRoundedRectangle(rect.GetX(), rect.GetY(), rect.GetWidth(), rect.GetHeight(), roundness);
+}
