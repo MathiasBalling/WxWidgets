875b485e442d0fc15f1f404b460b8ad545f21e2d
Mathias Balling <math260r@gmail.com>
2023-03-31 22:32:16 +0200

    31-03-2023 - 22:32

--- Header/ColorPane.h
+++ Header/ColorPane.h
@@ -12,16 +12,3 @@ public:
 private:
     virtual void DrawContent(wxGraphicsContext *gc, const wxRect &rect, int roundness) const override;
 };
-
-ColorPane::ColorPane(wxWindow *parent, wxWindowID id, const wxColor &paneColor, const wxPoint &pos, const wxSize &size)
-    : SelectablePane(parent, id, pos, size), color(paneColor)
-{
-}
-
-void ColorPane::DrawContent(wxGraphicsContext *gc, const wxRect &rect, int roundness) const
-{
-    gc->SetPen(wxPen(color));
-    gc->SetBrush(wxBrush(color));
-
-    gc->DrawRoundedRectangle(rect.GetX(), rect.GetY(), rect.GetWidth(), rect.GetHeight(), roundness);
-}
---
+++ Source/pensizepane.cpp
@@ -0,0 +1,22 @@
+#include "../Header/pensizepane.h"
+
+PenSizePane::PenSizePane(wxWindow *parent, wxWindowID id, int penW, const wxPoint &pos, const wxSize &size)
+    : SelectablePane(parent, id, pos, size), penWidth(penW)
+{
+}
+
+void PenSizePane::DrawContent(wxGraphicsContext *gc, const wxRect &rect, int roundness) const
+{
+    gc->SetPen(*wxWHITE_PEN);
+    gc->SetBrush(*wxWHITE_BRUSH);
+
+    gc->DrawRoundedRectangle(rect.GetX(), rect.GetY(), rect.GetWidth(), rect.GetHeight(), roundness);
+
+    gc->SetPen(*wxBLACK_PEN);
+    gc->SetBrush(*wxBLACK_BRUSH);
+
+    gc->DrawEllipse(rect.GetX() + rect.GetWidth() / 2 - penWidth / 2,
+                    rect.GetY() + rect.GetHeight() / 2 - penWidth / 2,
+                    penWidth,
+                    penWidth);
+}
